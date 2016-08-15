f288c64453042c80800fbd8d45973ffff1633551
lvdingguo <meng-zhilva@163.com>
2016-08-15 14:42:51 +0800

    chang

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+//框架的入口文件
+
+//开启调试模式：
+define("APP_DEBUG",true);
+
+//指定当前项目的应用目录名
+define("APP_PATH","./App/");
+
+//导入Thinkphp框架
+require("./ThinkPHP/ThinkPHP.php");
+
+
